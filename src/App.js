c1180089f670ed013de3fe844e35643be94be1b0
Logan Welty <12logan.welty@gmail.com>
2024-07-12 14:20:42 -0700

    This is my initial push.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// Goal is to make this front end and backend,
+// but the backend must use Django and create two api end points
+
+/*
+ *  (Square): This is my driver function
+ *  It can be exported and used by other files
+ */
+export default function Square() {
+  // This function returns a html node
+  // It is called JSX which is a combination of JavaScript and HTML
+  return <button className="square">X</button>;
+}
